a95b314ea6f7c9b4bf5f6831a5a9dbb4c50f9817
mandekhalane <122971562+mandekhalane@users.noreply.github.com>
2023-08-28 14:13:44 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <h1 style= "background-color:powderblue;"> My name is mandek </h1>
+        <p style = " color:red ;"> am trying to start learning coding and my lack of displine is really not helping </p>
+        <p> it really ffeels like your not even in control of your own mind that god gifted you </p>
+    </body>
+</html>
